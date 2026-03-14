4652b4a0ecab6b822b8b248b62c254419ac17c42
Sandro4433 <gablsandro@hotmail.com>
2026-03-14 10:17:38 +0100

    github repo test

--- python/Main.py
+++ python/Main.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import sys
 
 
+#This is a github repo test
+
 def _ensure_project_on_syspath() -> None:
     project_dir = Path(__file__).resolve().parent  # .../python
     if str(project_dir) not in sys.path:
